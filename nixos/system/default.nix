0b8a25af1819490e81ac32e2ee776e9da0c11ff9
amanfreecs <aman.hobo@gmail.com>
2026-04-06 00:08:23 +0530

    added nfs for remote storage

--- nixos/system/default.nix
+++ nixos/system/default.nix
@@ -2,6 +2,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "nfs" ];
 
   xdg.portal.enable = true;
   xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
